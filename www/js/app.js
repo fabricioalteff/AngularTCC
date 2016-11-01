8790594077f9035f6ec190a3a3465acf63c87dcc
Luiz Alexandre <luiz.alexandre@live.com>
2016-11-01 20:17:24 -0200

    angular init

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,14 @@
-angular.module("superCompras", ["ngRoute", "ngMaterial"]);
+var spa = angular.module('superCompras', ['ngRoute']);
+
+spa.config(function($routeProvider){
+    $routeProvider
+    .when('/', {
+        templateUrl: '../templates/login.html',
+        controller: 'loginCtrl'
+    })
+    .when('/venda', {
+        templateUrl: '../templates/venda.html',
+        controller: 'vendaCtrl'
+    })   
+    .otherwise({ redirectTo: '/'});
+});
