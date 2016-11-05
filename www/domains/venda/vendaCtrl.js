a7e2f334f5cc16235f66a0e137dca5a01576d3bd
Fabricio Alteff <fabricioalteff@hotmaiil.com>
2016-11-04 22:50:01 -0200

    Consumindo dados da API

--- www/domains/venda/vendaCtrl.js
+++ www/domains/venda/vendaCtrl.js
@@ -1,6 +1,19 @@
-angular.module("superCompras").controller("vendaCtrl", function($scope) {
+angular.module("superCompras").controller("vendaCtrl", function($scope, $http) {
   	$scope.app="Super Compras";
+    $scope.produtos = [];
   	console.log("está funcionando VENDA");
 
+    var carregarProdutos = function() {
+        $http.get('http://localhost:3412/produtos').success(function(data) {
+            $scope.produtos = data;
+
+            console.log(data);
+
+        }).error(function(data, status) {
+            $scope.message = "Aconteceu um problema: " + data;
+        });
+    };
+    carregarProdutos();
 
 });
+
