2961b8973c30834efa6aaf733578d5713b2c15ff
Fabricio Alteff <fabricioalteff@hotmaiil.com>
2016-11-07 21:18:34 -0200

    Inclusão do campo quantidade

--- www/domains/venda/vendaCtrl.js
+++ www/domains/venda/vendaCtrl.js
@@ -1,19 +1,16 @@
 angular.module("superCompras").controller("vendaCtrl", function($scope, $http) {
-  	$scope.app="Super Compras";
+    $scope.app = "Super Compras";
     $scope.produtos = [];
-  	console.log("está funcionando VENDA");
 
     var carregarProdutos = function() {
         $http.get('http://localhost:3412/produtos').success(function(data) {
             $scope.produtos = data;
 
-            console.log(data);
-
         }).error(function(data, status) {
             $scope.message = "Aconteceu um problema: " + data;
+            console.log($scope.message);
         });
     };
     carregarProdutos();
 
 });
-
